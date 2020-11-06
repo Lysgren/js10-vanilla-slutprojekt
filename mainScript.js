173ef4bc9c2f0e55ae591dfd8eb97d1db2369043
Lysgren <oscar.lysgren@gmail.com>
2020-11-06 10:37:46 +0100

    JavaScript

--- mainScript.js
+++ mainScript.js
@@ -1,33 +1,50 @@
 "use strict";
 
-let writeDataInDOM = (position, item) => {
-  let data = document.createElement("div");
-  data.innerHTML = item
-  position.append(data)
-}
-
 let getData = async requestedData => {
   let request = await fetch(requestedData)
   let data  = await request.json()
   return data
 }
 
-let renderBeer = async randomBeerNumber => {
-  let randomBeer = "https://api.punkapi.com/v2/beers/" + randomBeerNumber
+let writeDataInDOM = (position, item) => {
+  let data = document.createElement("div");
+  data.innerHTML = item
+  position.append(data)
+}
+
+let randomBeer = async () => {
+  let randomBeer = "https://api.punkapi.com/v2/beers/random"
 
   let beerData = await getData(randomBeer)
   console.log(beerData[0].name)
+  // console.log(beerData[0])
+}
+
+let beerSearch = async () => {
+  let value = document.querySelector("input.search-beer-input").value.toLowerCase()
+  value = value.replace(/\s/g, '_')
+
+  let searchedBeer = await getData("https://api.punkapi.com/v2/beers?beer_name=" + value)
+
+  for (let i = 0; i < searchedBeer.length; i++) {
+    console.log(searchedBeer[i].name)
+  }
+}
+
+let beerInfoPage = () => {
+  
 }
 
 let main = () => {
-  let randomBeerNumber = Math.floor((Math.random() * 325) + 1);
-  
   document.querySelector(".random-beer").addEventListener("click", function() {
-    randomBeerNumber = Math.floor((Math.random() * 325) + 1);
-    renderBeer(randomBeerNumber)
+    randomBeer()
   });
+
+  document.querySelector("input").oninput = function(event) {
+    beerSearch()
+  };
   
-  renderBeer(randomBeerNumber)
+  randomBeer()
 }
 
 main();
