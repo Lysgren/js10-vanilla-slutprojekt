a3f2a1e656864c114c4709aff13f7121df01d8dd
Lysgren <oscar.lysgren@gmail.com>
2020-11-06 13:02:06 +0100

    push

--- mainScript.js
+++ mainScript.js
@@ -41,22 +41,36 @@ let beerSearch = async () => {
 let beerInfoPage = beer => {
 
   console.log(beer[0])
-  //console.log(`Beer name: ${beer[0].name}`)
-  //console.log(`Beer description: ${beer[0].description}`)
-  //console.log(`Beer image(url): ${beer[0].image_url}`)
-  //console.log(`Beer alcohol by volume: ${beer[0].abv}`)
-  //console.log(`Beer volume: ${beer[0].volume.value} ${beer[0].volume.unit}`)
-  
-  //console.log(`Beer ingredients: ${beer[0].ingredients}`)
-  console.log(`Beer hops: ${beer[0].ingredients.hops}`)
-  console.log("Beer hops: " + beer[0].ingredients.hops)
-  console.log(beer[0].ingredients.hops.length)
-  console.log(beer[0].ingredients.hops[0])
+  console.log(`Beer name: ${beer[0].name}`)
+  console.log(`Beer description: ${beer[0].description}`)
+  console.log(`Beer image(url): ${beer[0].image_url}`)
+  console.log(`Beer alcohol by volume: ${beer[0].abv}`)
+  console.log(`Beer volume: ${beer[0].volume.value} ${beer[0].volume.unit}`)
 
-  //console.log("Beer ingredients: " + beer[0].ingredients)
+  let ingredients = []
+  if (beer[0].ingredients.malt[0].name != undefined) {
+    console.log("There is malt!")
+    for (let i = 0; i < beer[0].ingredients.hops.length; i++) {
+      ingredients.push(beer[0].ingredients.malt[i].name)
+    }
+  }
 
-  //console.log(`Beer food pairing: ${beer[0].food_pairing}`)
-  //console.log(`Beer brewers tips: ${beer[0].brewers_tips}`)
+  if (beer[0].ingredients.yeast != undefined) {
+    ingredients.push(beer[0].ingredients.yeast)
+  }
+
+  console.log(`Beer ingredients: ${ingredients}`)
+
+  let hops = []
+  if (beer[0].ingredients.hops[0].name != undefined) {
+    for (let j = 0; j < beer[0].ingredients.hops.length; j++) {
+      hops.push(beer[0].ingredients.hops[j].name)
+    }
+  }
+
+  console.log(`Beer hops: ${hops}`)
+  console.log(`Beer food pairing: ${beer[0].food_pairing}`)
+  console.log(`Beer brewers tips: ${beer[0].brewers_tips}`)
 }
 
 let main = () => {
