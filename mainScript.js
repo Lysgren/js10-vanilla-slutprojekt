71408b120b37026affa3becbf0ceddee6e0f5d1b
Lysgren <oscar.lysgren@gmail.com>
2020-11-06 11:26:31 +0100

    Beerinfopage function

--- mainScript.js
+++ mainScript.js
@@ -16,12 +16,15 @@ let randomBeer = async () => {
   let randomBeer = "https://api.punkapi.com/v2/beers/random"
 
   let beerData = await getData(randomBeer)
-  console.log(beerData[0].name)
+  
+  // console.log(beerData[0].name)
   // console.log(beerData[0])
 
-  //document.querySelectorAll(".beer-name").innerHTML=beerData[0].name
-  // document.querySelectorAll(".beer-img").src=beerData[0].image_url
-  // document.querySelector(".description").innerHTML=beerData[0].description
+  document.querySelectorAll(".beer-name").innerHTML=beerData[0].name
+  document.querySelectorAll(".beer-img").src=beerData[0].image_url
+  document.querySelector(".description").innerHTML=beerData[0].description
+
+  beerInfoPage(beerData)
 }
 
 let beerSearch = async () => {
@@ -35,8 +38,25 @@ let beerSearch = async () => {
   }
 }
 
-let beerInfoPage = () => {
+let beerInfoPage = beer => {
+
+  console.log(beer[0])
+  //console.log(`Beer name: ${beer[0].name}`)
+  //console.log(`Beer description: ${beer[0].description}`)
+  //console.log(`Beer image(url): ${beer[0].image_url}`)
+  //console.log(`Beer alcohol by volume: ${beer[0].abv}`)
+  //console.log(`Beer volume: ${beer[0].volume.value} ${beer[0].volume.unit}`)
   
+  //console.log(`Beer ingredients: ${beer[0].ingredients}`)
+  console.log(`Beer hops: ${beer[0].ingredients.hops}`)
+  console.log("Beer hops: " + beer[0].ingredients.hops)
+  console.log(beer[0].ingredients.hops.length)
+  console.log(beer[0].ingredients.hops[0])
+
+  //console.log("Beer ingredients: " + beer[0].ingredients)
+
+  //console.log(`Beer food pairing: ${beer[0].food_pairing}`)
+  //console.log(`Beer brewers tips: ${beer[0].brewers_tips}`)
 }
 
 let main = () => {
