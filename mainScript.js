27bfe40856fccf54700c530938e931b2144de3d2
Nilufar1026 <71069308+Nilufar1026@users.noreply.github.com>
2020-11-09 14:14:54 +0100

    html,css

--- mainScript.js
+++ mainScript.js
@@ -9,7 +9,7 @@ let getData = async requestedData => {
 let writeDataInDOM = (requestedElement, str) => {
   let element = document.querySelector(requestedElement);
   element.innerHTML = str
-  // element.src=
+  
 }
 
 let randomBeer = async () => {
@@ -49,7 +49,7 @@ let beerInfoPage = beer => {
     hops.push(beer[0].ingredients.hops[j].name)
   }
 
-  writeDataInDOM(".beer-name-info", `Name:${beer[0].name}`)
+  writeDataInDOM(".beer-name-info",beer[0].name)
   writeDataInDOM(".description", `Description:${beer[0].description}`)
   writeDataInDOM(".alcohol-by-volume", `Beer alcohol by volume: ${beer[0].abv}`)
   writeDataInDOM(".volume", `Beer volume: ${beer[0].volume.value} ${beer[0].volume.unit}`)
@@ -57,7 +57,9 @@ let beerInfoPage = beer => {
   writeDataInDOM(".hops", `Beer hops: ${hops}`)
   writeDataInDOM(".food-pairing", `Beer food pairing: ${beer[0].food_pairing}`)
   writeDataInDOM(".brewers-tips", `Beer brewers tips: ${beer[0].brewers_tips}`)
-  writeDataInDOM(".beer-img-info", beer[0].image_url)
+  // let imgSrc=document.querySelector(".beer-img-info")
+  // imgSrc.src=beer[0].image_url
+  
 }
 
 let main = () => {
